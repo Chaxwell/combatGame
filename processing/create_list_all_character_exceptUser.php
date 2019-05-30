ab40a574168030948d7078dbad6cceb8f58e9949
HeathSilcox <chaxel.christophe@gmail.com>
2019-05-30 04:14:05 +0200

    Adjusting code

--- processing/create_list_all_character_exceptUser.php
+++ processing/create_list_all_character_exceptUser.php
@@ -2,18 +2,15 @@
 
 // create_list_all_character_exceptUser.php
 
-
-$charManager = new CharacterManager;
 $listCharacters = $charManager->getAllCharactersExceptConnectedUser($_SESSION['userId']);
 ?>
 
-<form>
-	<?php foreach ($listCharacters as $character) : ?>
-		<div class="list-group-item list-group-item-action">
-			<input type="radio" id="<?= $character['id']; ?>" name="perso">
-			<label for="<?= $character['id']; ?>">
-				<?= $character['name'] . ' - ' . $character['class'] . ' - ' . $character['healthPoints'] . 'pdv'; ?>
-			</label>
-		</div>
-	<?php endforeach; ?>
-</form>
+
+<?php foreach ($listCharacters as $character) : ?>
+	<div class="list-group-item list-group-item-action">
+		<input type="radio" id="<?= $character['id']; ?>" name="perso">
+		<label for="<?= $character['id']; ?>">
+			<?= $character['name'] . ' - ' . $character['class'] . ' - ' . $character['healthPoints'] . 'pdv'; ?>
+		</label>
+	</div>
+<?php endforeach; ?>
--- processing/create_list_characters_connected_user.php
+++ processing/create_list_characters_connected_user.php
@@ -2,17 +2,16 @@
 
 // create_list_characters_connected_user.php
 
-$charManager = new CharacterManager;
 $listCharacters = $charManager->getCharactersFromUser($_SESSION['userId']);
 ?>
 
 <!-- <label for="pet-select">Mes personnages</label> -->
 <select id="pet-select" class="list-group-item list-group-item-action">
-    <option value="">--Choisis un de tes persos pour attaquer--</option>
-    <?php foreach($listCharacters as $character): ?>
-	<option value="<?=$character['id'];?>">
-	    <?=$character['name'];?> - <?=$character['class'];?> - <?=$character['healthPoints'];?> pdv
-	    - Force de <?=$character['strength'];?>
-	</option>
+	<option value="">--Choisis un de tes persos pour attaquer--</option>
+	<?php foreach ($listCharacters as $character) : ?>
+		<option value="<?= $character['id']; ?>">
+			<?= $character['name']; ?> - <?= $character['class']; ?> - <?= $character['healthPoints']; ?> pdv
+			- Force de <?= $character['strength']; ?>
+		</option>
 	<?php endforeach ?>
 </select>
